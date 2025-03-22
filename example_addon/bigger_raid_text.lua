526e59c38defc0002dd3e536b692231c0d8268d2
sjwayrhz <sjwayrhz@gmail.com>
2025-03-23 03:52:37 +0800

    delete some parts

--- example_addon/bigger_raid_text.lua
+++
@@ -1,9 +0,0 @@
--- This is perhaps the simplest example we can provide. This just makes the font size bigger for Commander chat.
--- Here we set it to 24 and it becomes very large!
--- As you can see, it grabs an existing window
-
-local raidCommandMessage = ADDON:GetContent(UIC.RAID_COMMAND_MESSAGE)
-raidCommandMessage.style:SetFontSize(15)
-
--- If you need to learn, you could try to make it a setting instead of a static value. 
--- That would be a cool addon people would love :)
